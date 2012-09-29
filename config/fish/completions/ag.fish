21335f72c1e360e8366e609b466e0f0a26df932c
Claudio Ortolina <claudio@jumpzero.com>
2012-09-29 15:55:33 +0100

    Test for tag file existence in ag completion.

--- config/fish/completions/ag.fish
+++ config/fish/completions/ag.fish
@@ -1,5 +1,9 @@
 function __cache_or_get_ag_completion -d "Create ag completions"
-  cut -f 1 .tags | uniq | grep -v '!_'
+
+  if test -f .tags
+    cut -f 1 .tags | uniq | grep -v '!_'
+  end
+
 end
 
 complete -x -c ag -a "(__cache_or_get_ag_completion)" --description 'Ctag'
