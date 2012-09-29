8844a22c958c2a8ef3131e2a0bd8a6285baa189e
Claudio Ortolina <claudio@jumpzero.com>
2012-09-29 16:05:19 +0100

    Adds ruby version to prompt and removes hostname.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -50,13 +50,13 @@ function parse_svn_revision
 end
 
 function fish_prompt -d "Write out the prompt"
-	printf '%s%s@%s%s' (set_color brown) (whoami) (hostname|cut -d . -f 1) (set_color normal)
+	# printf '%s%s@%s%s' (set_color brown) (whoami) (hostname|cut -d . -f 1) (set_color normal)
 
 	# Color writeable dirs green, read-only dirs red
 	if test -w "."
-		printf ' %s%s' (set_color green) (prompt_pwd)
+		printf '%s%s' (set_color green) (prompt_pwd)
 	else
-		printf ' %s%s' (set_color red) (prompt_pwd)
+		printf '%s%s' (set_color red) (prompt_pwd)
 	end
 
         # Print subversion tag or branch
@@ -74,5 +74,7 @@ function fish_prompt -d "Write out the prompt"
     prompt_git
 		# printf ' %s%s/%s' (set_color normal) (set_color blue) (parse_git_branch)
 	end
-	printf '%s ± ' (set_color normal)
+
+  printf ' %s%s%s ± ' (set_color blue) (rvm-prompt v) (set_color normal)
+
 end
