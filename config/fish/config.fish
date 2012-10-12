8339cc4840bcda5ad8de91b4be0156b6c471b968
Claudio Ortolina <claudio@jumpzero.com>
2012-10-12 13:12:26 +0100

    Small fixes.
    Signed-off-by: Claudio Ortolina <claudio@jumpzero.com>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,17 @@
 if status --is-login
-	for p in /usr/bin /usr/local/bin /opt/local/bin /usr/local/mysql/bin /opt/local/lib/postgresql83/bin ~/bin ~/.config/fish/bin
-		if test -d $p
-			set PATH $p $PATH
-		end
-	end
+
+  for p in /usr/local/bin /usr/local/sbin /usr/local/share/npm/bin /usr/bin /opt/local/bin ~/.config/fish/bin
+    if test -d $p
+      set PATH $p $PATH
+    end
+  end
+
+  # script that needs to start once per fishd session
+  if test $RUNNING_TMUX_COPY == false
+    tmux_copy &
+    set -U RUNNING_TMUX_COPY true
+  end
+
 end
 
 # Load custom settings for current hostname
@@ -38,43 +46,43 @@ set fish_greeting ""
 set -x CLICOLOR 1
 
 function parse_git_branch
-	sh -c 'git branch --no-color 2> /dev/null' | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+  sh -c 'git branch --no-color 2> /dev/null' | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 end
 
 function parse_svn_tag_or_branch
-        sh -c 'svn info | grep "^URL:" | egrep -o "(tags|branches)/[^/]+|trunk" | egrep -o "[^/]+$"'
+  sh -c 'svn info | grep "^URL:" | egrep -o "(tags|branches)/[^/]+|trunk" | egrep -o "[^/]+$"'
 end
 
 function parse_svn_revision
-	sh -c 'svn info 2> /dev/null' | sed -n '/^Revision/p' | sed -e 's/^Revision: \(.*\)/\1/'
+  sh -c 'svn info 2> /dev/null' | sed -n '/^Revision/p' | sed -e 's/^Revision: \(.*\)/\1/'
 end
 
 function fish_prompt -d "Write out the prompt"
-	# printf '%s%s@%s%s' (set_color brown) (whoami) (hostname|cut -d . -f 1) (set_color normal)
+# printf '%s%s@%s%s' (set_color brown) (whoami) (hostname|cut -d . -f 1) (set_color normal)
 
-	# Color writeable dirs green, read-only dirs red
-	if test -w "."
-		printf '%s%s' (set_color brown) (prompt_pwd)
-	else
-		printf '%s%s' (set_color red) (prompt_pwd)
-	end
+# Color writeable dirs green, read-only dirs red
+if test -w "."
+  printf '%s%s' (set_color brown) (prompt_pwd)
+else
+  printf '%s%s' (set_color red) (prompt_pwd)
+end
 
-        # Print subversion tag or branch
-        if test -d ".svn"
-                printf ' %s%s%s' (set_color normal) (set_color blue) (parse_svn_tag_or_branch)
-        end
+# Print subversion tag or branch
+if test -d ".svn"
+  printf ' %s%s%s' (set_color normal) (set_color blue) (parse_svn_tag_or_branch)
+end
 
-	# Print subversion revision
-	if test -d ".svn"
-		printf '%s%s@%s' (set_color normal) (set_color blue) (parse_svn_revision)
-	end
+# Print subversion revision
+if test -d ".svn"
+  printf '%s%s@%s' (set_color normal) (set_color blue) (parse_svn_revision)
+end
 
-	# Print git branch
-	if test -d ".git"
-    prompt_git
-		# printf ' %s%s/%s' (set_color normal) (set_color blue) (parse_git_branch)
-	end
+# Print git branch
+if test -d ".git"
+  prompt_git
+  # printf ' %s%s/%s' (set_color normal) (set_color blue) (parse_git_branch)
+end
 
-  printf ' %s%s%s ± %s' (set_color green) (rvm-prompt v) (set_color brown) (set_color normal)
+printf ' %s%s%s ± %s' (set_color green) (rvm-prompt v) (set_color brown) (set_color normal)
 
 end
