bd727d66759ecc5fd68e1be7c4d5e7f176bb61cd
Claudio Ortolina <claudio@pspdfkit.com>
2023-03-13 09:05:38 +0000

    [Git] Add global configuration

--- home.nix
+++ home.nix
@@ -10,6 +10,69 @@
     (pkgs.nerdfonts.override { fonts = ["JetBrainsMono"]; })
   ];
 
+  programs.git = {
+    enable = true;
+    userName = "Claudio Ortolina";
+    userEmail = "cloud8421@gmail.com";
+
+    aliases = {
+      co = "checkout";
+      pick = "cherry-pick";
+      s = "status --short";
+      pr = "pull --rebase";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --";
+    };
+
+    ignores = [ ".DS_Store" ];
+    attributes = [
+      "*.c     diff=cpp"
+      "*.h     diff=cpp"
+      "*.c++   diff=cpp"
+      "*.h++   diff=cpp"
+      "*.cpp   diff=cpp"
+      "*.hpp   diff=cpp"
+      "*.cc    diff=cpp"
+      "*.hh    diff=cpp"
+      "*.cs    diff=csharp"
+      "*.css   diff=css"
+      "*.html  diff=html"
+      "*.xhtml diff=html"
+      "*.ex    diff=elixir"
+      "*.exs   diff=elixir"
+      "*.go    diff=golang"
+      "*.php   diff=php"
+      "*.pl    diff=perl"
+      "*.py    diff=python"
+      "*.md    diff=markdown"
+      "*.rb    diff=ruby"
+      "*.rake  diff=ruby"
+      "*.rs    diff=rust"
+      "*.lisp  diff=lisp"
+      "*.el    diff=lisp"
+      "*.clj   diff=lisp"
+      "*.cljs  diff=lisp"
+      "*.cljc  diff=lisp"
+    ];
+
+    extraConfig = {
+      color.branch = "auto";
+      color.diff = "auto";
+      color.interactive = "auto";
+      color.status = "auto";
+      commit.verbose = "true";
+      core.editor = "nvim";
+      format.pretty = "%Cblue%h%Creset %Cgreen[%ar]%Creset (%an) %s";
+      pull.rebase = "false";
+      push.default = "current";
+      status.branch = 1;
+      status.short = 1;
+    };
+
+    lfs = {
+      enable = true;
+    };
+  };
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
