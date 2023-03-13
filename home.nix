0c0390d6d94478ff10347a64c3e8763514099bb8
Claudio Ortolina <claudio@pspdfkit.com>
2023-03-13 09:05:38 +0000

    [Nvim, Starship, Tmux] Install with base config

--- home.nix
+++ home.nix
@@ -6,6 +6,69 @@
   
   home.packages = [
     pkgs.git
-    pkgs.neovim
   ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+
+  programs.starship = {
+    enable = true;
+
+    settings = {
+      add_newline = false;
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    shortcut = "a";
+    baseIndex = 1;
+
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      pain-control
+      copycat
+      yank
+      open
+      battery
+      prefix-highlight
+      {
+        plugin = dracula;
+	extraConfig = ''
+          set -g @dracula-show-fahrenheit false
+          set -g @dracula-show-battery false
+          set -g @dracula-show-network false
+          set -g @dracula-show-weather false
+	'';
+      }
+    ];
+
+    extraConfig = ''
+      # Cycle through panes
+      unbind ^A
+      bind ^A select-pane -t :.+
+      # Copy mode works as Vim
+      unbind [
+      bind Escape copy-mode
+      unbind p
+      bind p paste-buffer
+
+      # Renumber windows when adding a new one
+      set -g renumber-windows on
+
+      # Mouse support
+      set -g mouse on
+    '';
+  };
+
 }
