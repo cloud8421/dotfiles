359c22bd56ca401926f370c68a46c96ca2c35442
Claudio Ortolina <claudio@pspdfkit.com>
2023-03-13 15:03:45 +0000

    [Nvim] QOL configuration changes

--- home.nix
+++ home.nix
@@ -151,8 +151,26 @@
     extraLuaConfig = ''
       vim.g.mapleader = ' '
       vim.g.maplocalleader = ' '
+      -- Show proper colors in terminal
       vim.o.termguicolors = true
       vim.cmd.colorscheme 'onedark'
+      -- Share clipboard with MacOS
+      vim.o.clipboard = 'unnamed'
+      -- Set highlight on search
+      vim.o.hlsearch = false
+      -- Make line numbers default
+      vim.wo.number = true
+      -- Enable mouse mode
+      vim.o.mouse = 'a'
+      -- Case insensitive searching UNLESS /C or capital in search
+      vim.o.ignorecase = true
+      vim.o.smartcase = true
+      -- Decrease update time
+      vim.o.updatetime = 250
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      -- Set completeopt to have a better completion experience
+      vim.o.completeopt = 'menuone,noselect'
     '';
 
   };
