82da54330b26985f587732179013a7fdd330fb1f
Claudio Ortolina <claudio@pspdfkit.com>
2023-03-17 18:55:18 +0000

    [Nvim] Format file on save (requires LSP)

--- home.nix
+++ home.nix
@@ -114,6 +114,7 @@ in
       cmp-nvim-lsp
       en-masse
       luasnip
+      lsp-format-nvim
       mason-lspconfig-nvim
       mason-nvim
       neodev-nvim
@@ -282,13 +283,19 @@ in
         -- EnMasse
         vim.keymap.set('n', '<leader>e', ":EnMasse<cr>")
 
+        -- LSP Format on save
+        require("lsp-format").setup {}
+
         -- LSP settings.
         --  This function gets run when an LSP connects to a particular buffer.
-        local on_attach = function(_, bufnr)
+        local on_attach = function(client, bufnr)
           -- NOTE: Remember that lua is a real programming language, and as such it is possible
           -- to define small helper and utility functions so you don't have to repeat yourself
           -- many times.
-          --
+
+          -- Format on save
+          require("lsp-format").on_attach(client)
+
           -- In this case, we create a function that lets us more easily define mappings specific
           -- for LSP related items. It sets the mode, buffer and description for us each time.
           local nmap = function(keys, func, desc)
