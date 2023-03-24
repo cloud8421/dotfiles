3f256649db6b3f6cc1b9c015e0ee8381a1fe4a88
Claudio Ortolina <claudio@pspdfkit.com>
2023-03-24 16:43:15 +0000

    [Nvim] Add fd
    Recommended as a dependency for Telescope

--- home.nix
+++ home.nix
@@ -24,6 +24,7 @@ in
     pkgs.postgresql
     pkgs.fzf
     pkgs.ripgrep
+    pkgs.fd
     # TODO: Requires running 'rustup default stable' - try to incorporate in home.nix
     pkgs.rustup
     pkgs.deno
