4622d667e22da340286c56caea51b02f406917c0
Claudio Ortolina <cloud8421@gmail.com>
2025-01-21 18:49:45 +0000

    Gruvbox (dark) all the things

---
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      contrast = "dark",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
