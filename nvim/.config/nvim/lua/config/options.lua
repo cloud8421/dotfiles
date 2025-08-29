06ec4d6beef6206c9b484e325475bc1673a0911d
Claudio Ortolina <cloud8421@gmail.com>
2025-08-29 17:50:06 +0300

    Configure expert ls

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,17 @@
 -- Add any additional options here
 
 vim.opt.listchars = "eol:↵,trail:~,tab:>-,nbsp:␣"
+
+vim.lsp.config("expert", {
+  cmd = { "expert" },
+  root_markers = { "mix.exs", ".git" },
+  filetypes = { "elixir", "eelixir", "heex", "surface" },
+  settings = {
+    expert = {
+      enableDiagnostics = true,
+      enableFormatting = true,
+    },
+  },
+})
+
+vim.lsp.enable("expert")
