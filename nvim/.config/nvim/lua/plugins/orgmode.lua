7daf86890d1a733770f154543cf840b909d2d507
Claudio Ortolina <cloud8421@gmail.com>
2026-08-05 07:12:30 +0100

    Add neovim Org mode templates

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -6,6 +6,15 @@ return {
       org_agenda_files = "~/Dropbox/org/**/*",
       org_default_notes_file = "~/Dropbox/org/refile.org",
       org_todo_keywords = { "TODO(t)", "|", "CANCELED(c)", "DONE(d)" },
+      org_startup_folded = "content",
+      org_capture_templates = {
+        j = {
+          description = "Personal",
+          template = "* %U\n%?",
+          target = "~/Dropbox/org/Journal/journal.org",
+          datetree = { tree_type = "day" },
+        },
+      },
     })
     -- Experimental LSP support
     vim.lsp.enable("org")
